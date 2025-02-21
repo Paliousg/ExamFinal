a608e949b73bb416c0e347425b727946ce66fcf9
Paliousg <amine.kad98@gmail.com>
2025-02-21 13:46:41 -0500

    first commit

---
+++ src/main/java/com/mycompany/examfinal/ExamFinal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.examfinal;
+
+/**
+ *
+ * @author 2417010
+ */
+public class ExamFinal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
